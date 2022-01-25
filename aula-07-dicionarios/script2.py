1522d4e3143d0ec5c911781e439ed6584334f339
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-01-24 21:19:52 -0300

    update

---
+++ aula-07-dicionarios/script2.py
@@ -0,0 +1,20 @@
+#import da função
+from managerUsers import *
+#dicionario de usuarios 
+usuarios ={}
+
+#instruções de acesso
+opcao = ask()
+
+#loop de acesso
+while opcao != "S":
+    if opcao == "I":
+      opcao = ask()
+        inserir(usuarios)
+         
+
+        
+        
+        
+    
+    
